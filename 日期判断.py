15ff4d62d1adb58b569bfff2409d9daa02560faf
pwq334107948 <91314084+pwq334107948@users.noreply.github.com>
2021-09-25 15:10:22 +0800

    Update 日期判断.py

--- 日期判断.py
+++ 日期判断.py
@@ -5,37 +5,50 @@
 @FileName:日期判断.py
 '''
 
-def is_leap_year(year):#判断是否是闰年
+def leap_year(year):#判断闰年
     if year%400==0 or year%4==0 and year%100!=0:
         return True
     else:
         return False
-def get_num_of_days_in_month(year,month):#判断输入月份有多少天
+def days_in_month(year,month):#每个月有多少天
     if month in (1,3,5,7,8,10,12):
-        day=31
+        return 31
     elif month in (4,6,9,11):
-        day=30
-    elif is_leap_year(year):
-        day=29
+        return 30
+    elif leap_year(year):
+        return 29
     else:
-        day=28
-    return day
-def get_total_num_of_days(year,month):#距离1800年1月1日有多少天
+        return 28
+def total_days(year,month):#距离给定的条件1900年1月1日有多少天
     total_days=0
-    for y in range(1800,year):
-        if is_leap_year(y):
+    for y in range(1900,year):
+        if leap_year(y):
             total_days+=366
         else:
             total_days+=365
     for m in range(1,month):
-        total_days+=get_num_of_days_in_month(year,m)
+        total_days+=days_in_month(year,m)
     return total_days
-def get_start_day(year,month):#获得起始星期,0代表星期天
-    return (3+get_total_num_of_days(year,month))%7
- 
+def start_day(year,month):#判断输入月份的起始星期
+    start_day=(1+total_days(year,month))%7
+    if start_day==0:
+        return '星期天'
+    elif start_day==1:
+        return '星期一'
+    elif start_day==2:
+        return '星期二'
+    elif start_day==3:
+        return '星期三'
+    elif start_day==4:
+        return '星期四'
+    elif start_day==5:
+        return '星期五'
+    else:
+        return '星期六'
+
 year=int(input('请输入年份：'))
 month=int(input('请输入月份：'))
 if year<=1800 or month<1 or month>12:
     print('输入有误！')
 else:
-    print(get_start_day(year,month))
+    print(year,'年',month,'月的第一天是',start_day(year,month))
