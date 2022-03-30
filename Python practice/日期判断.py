e5a4a45ff50d9c53be0a8067613d5143e9dd1c39
pwq334107948 <91314084+pwq334107948@users.noreply.github.com>
2022-03-30 22:00:00 +0800

    Modified

--- Python practice/日期判断.py
+++ Python practice/日期判断.py
@@ -5,12 +5,12 @@
 @FileName:日期判断.py
 '''
 
-def leap_year(year):#判断闰年
+def leap_year(year): #判断闰年
     if year%400==0 or year%4==0 and year%100!=0:
         return True
     else:
         return False
-def days_in_month(year,month):#每个月有多少天
+def days_in_month(year,month): #每个月有多少天
     if month in (1,3,5,7,8,10,12):
         return 31
     elif month in (4,6,9,11):
@@ -19,7 +19,7 @@ def days_in_month(year,month):#每个月有多少天
         return 29
     else:
         return 28
-def total_days(year,month):#距离给定的条件1900年1月1日有多少天
+def total_days(year,month): #距离给定的条件1900年1月1日有多少天
     total_days=0
     for y in range(1900,year):
         if leap_year(y):
@@ -29,7 +29,7 @@ def total_days(year,month):#距离给定的条件1900年1月1日有多少天
     for m in range(1,month):
         total_days+=days_in_month(year,m)
     return total_days
-def start_day(year,month):#判断输入月份的起始星期
+def start_day(year,month): #判断输入月份的起始星期
     start_day=(1+total_days(year,month))%7
     if start_day==0:
         return '星期天'
@@ -46,9 +46,9 @@ def start_day(year,month):#判断输入月份的起始星期
     else:
         return '星期六'
 
-year=int(input('请输入年份：'))
-month=int(input('请输入月份：'))
-if year<=1800 or month<1 or month>12:
+year = int(input('请输入年份：'))
+month = int(input('请输入月份：'))
+if year <= 1800 or month < 1 or month > 12:
     print('输入有误！')
 else:
     print(year,'年',month,'月的第一天是',start_day(year,month))
